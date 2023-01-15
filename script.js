d3a8941010efad3dca7e3345ed8f5b1e53ebab2b
Sebastian-123-web <sebastian09_26@hotmail.com>
2023-01-15 13:02:59 -0500

    update menuburger

--- script.js
+++ script.js
@@ -2,15 +2,23 @@
 const menuBurger = document.querySelector('.menu-burger');
 const closeBurger = document.querySelector('.close-burger');
 const nevagacion = document.querySelector('#nevagacion');
+const a = document.querySelectorAll('a');
+
 menuBurger.onclick = function(){
     nevagacion.style.display = "flex";
     closeBurger.style.zIndex = "9";
     closeBurger.style.display = "flex";
 }
-closeBurger.onclick = function(){
+
+const closeA = () => {
     nevagacion.style.display = "none";
     closeBurger.style.display = "none";
     nevagacion.style.justifyContent = "center";
     nevagacion.style.alignItems = "center"
-    console.log(screen.width)
 }
+
+closeBurger.onclick = closeA;
+
+a[1].onclick = closeA;
+a[2].onclick = closeA;
+a[3].onclick = closeA;
